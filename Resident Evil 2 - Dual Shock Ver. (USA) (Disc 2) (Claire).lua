a74b018665d32581ed28489cb8e3961d8a4325ba
DejaSentj <43799369+DejaSentj@users.noreply.github.com>
2022-08-10 16:10:50 +1200

    Add files via upload

---
+++ Resident Evil 2 - Dual Shock Ver. (USA) (Disc 2) (Claire).lua
@@ -0,0 +1,16 @@
+-- Resident Evil 2 - Dual Shock Ver. (USA) (Disc 2) (Clare)
+-- Cheats
+-- ported to PS4 lua
+-- emu used=syphonfilter v1
+
+
+local patcher = function()
+
+--[Enable Infinite Ammo (In-Game cheat)]
+R3K_WriteMem16(0x800D4684,0xFF00)
+R3K_WriteMem16(0x800D4686,0xFFFF)
+--[Infinite Health]
+R3K_WriteMem16(0x800CFD06,0x00C8)
+end
+
+EM_AddVsyncHook(patcher)
